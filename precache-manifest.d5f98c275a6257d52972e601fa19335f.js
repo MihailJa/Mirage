b82cd9b341b032d53979ffcb78820c34631b21c9
Game <jazzja891@gmail.com>
2020-08-26 18:02:07 +0300

    Updates

---
+++ precache-manifest.d5f98c275a6257d52972e601fa19335f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85680ee45c5a36aaa6c2d57bcd543f40",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "9ce6f5858f507da64c1a",
+    "url": "/react-app/static/css/2.fab5e3fc.chunk.css"
+  },
+  {
+    "revision": "55ba6f45f1a565424be1",
+    "url": "/react-app/static/css/3.74650f16.chunk.css"
+  },
+  {
+    "revision": "150595bdef49bcd9c9e5",
+    "url": "/react-app/static/css/4.fc62ac51.chunk.css"
+  },
+  {
+    "revision": "cb71ffe85475c2e03194",
+    "url": "/react-app/static/css/main.f3ef71ae.chunk.css"
+  },
+  {
+    "revision": "9ce6f5858f507da64c1a",
+    "url": "/react-app/static/js/2.10ec8c3e.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/react-app/static/js/2.10ec8c3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55ba6f45f1a565424be1",
+    "url": "/react-app/static/js/3.dfb04bf5.chunk.js"
+  },
+  {
+    "revision": "150595bdef49bcd9c9e5",
+    "url": "/react-app/static/js/4.5e8d4146.chunk.js"
+  },
+  {
+    "revision": "cb71ffe85475c2e03194",
+    "url": "/react-app/static/js/main.7c259c78.chunk.js"
+  },
+  {
+    "revision": "b78645e9340320f0e992",
+    "url": "/react-app/static/js/runtime-main.31c16182.js"
+  },
+  {
+    "revision": "5589eda272c003a7c45eec841b5cd50e",
+    "url": "/react-app/static/media/dzone.5589eda2.svg"
+  },
+  {
+    "revision": "cb55ad9bfe654e724d4ca18b381cbbbc",
+    "url": "/react-app/static/media/preloader.cb55ad9b.svg"
+  },
+  {
+    "revision": "c1c6129c795a780eba921ca4a86ff2bb",
+    "url": "/react-app/static/media/user.c1c6129c.png"
+  }
+]);
